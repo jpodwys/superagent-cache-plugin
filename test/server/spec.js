38f1a2a1cff6470f522f624557a74171225d4c2f
Joseph Podwys <joseph@podwys.com>
2016-12-18 01:17:10 -0700

    Adding more unit tests.

--- test/server/spec.js
+++ test/server/spec.js
@@ -11,8 +11,24 @@ app.get('/one', function(req, res){
   res.send(200, {key: 'one'});
 });
 
-app.get('/four', function(req, res){
-  res.send(400, {key: 'one'});
+app.post('/one', function(req, res){
+  res.send(200, {key: 'post'});
+});
+
+app.put('/one', function(req, res){
+  res.send(200, {key: 'put'});
+});
+
+app.patch('/one', function(req, res){
+  res.send(200, {key: 'patch'});
+});
+
+app.delete('/one', function(req, res){
+  res.send(200, {key: 'delete'});
+});
+
+app.get('/redirect', function(req, res){
+  res.redirect('/one');
 });
 
 app.listen(3000);
@@ -35,98 +51,106 @@ describe('superagentCache', function(){
       );
     });
 
-    // it('.post() .end() should bypass all caching logic', function (done) {
-    //   superagent
-    //     .post('localhost:3000/one')
-    //     .end(function (err, response, key){
-    //       expect(response.body.key).toBe('post');
-    //       superagentCache.cache.get(key, function (err, response) {
-    //         expect(response).toBe(null);
-    //         done();
-    //       });
-    //     }
-    //   );
-    // });
+    it('.post() .end() should bypass all caching logic', function (done) {
+      superagent
+        .post('localhost:3000/one')
+        .use(superagentCache)
+        .end(function (err, response, key){
+          expect(response.body.key).toBe('post');
+          cache.get(key, function (err, response) {
+            expect(response).toBe(null);
+            done();
+          });
+        }
+      );
+    });
 
-    // it('.get(redirect) .end() should cache the result of the redirect using the original request\'s key', function (done) {
-    //   superagent
-    //     .get('http://localhost:3000/redirect')
-    //     .end(function (err, response, key){
-    //       expect(key).toBe('{"method":"GET","uri":"http://localhost:3000/redirect","params":null,"options":{}}');
-    //       expect(response.body.key).toBe('one');
-    //       superagentCache.cache.get(key, function (err, response) {
-    //         expect(response.body.key).toBe('one');
-    //         done();
-    //       });
-    //     }
-    //   );
-    // });
+    it('.get(redirect) .end() should cache the result of the redirect using the original request\'s key', function (done) {
+      superagent
+        .get('http://localhost:3000/redirect')
+        .use(superagentCache)
+        .end(function (err, response, key){
+          expect(key).toBe('{"method":"GET","uri":"http://localhost:3000/redirect","params":null,"options":{}}');
+          expect(response.body.key).toBe('one');
+          cache.get(key, function (err, response) {
+            expect(response.body.key).toBe('one');
+            done();
+          });
+        }
+      );
+    });
 
-    // it('.get() then .put() should invalidate cache', function (done) {
-    //   superagent
-    //     .get('localhost:3000/one')
-    //     .end(function (err, response, key){
-    //       expect(response.body.key).toBe('one');
-    //       superagent.cache.get(key, function (err, response) {
-    //         expect(response.body.key).toBe('one');
-    //         superagent
-    //           .put('localhost:3000/one')
-    //           .end(function (err, response, key){
-    //             expect(response.body.key).toBe('put');
-    //             superagent.cache.get(key, function (err, response) {
-    //               expect(response).toBe(null);
-    //               done();
-    //             });
-    //           }
-    //         );
-    //       });
-    //     }
-    //   );
-    // });
+    it('.get() then .put() should invalidate cache', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .use(superagentCache)
+        .end(function (err, response, key){
+          expect(response.body.key).toBe('one');
+          cache.get(key, function (err, response) {
+            expect(response.body.key).toBe('one');
+            superagent
+              .put('localhost:3000/one')
+              .use(superagentCache)
+              .end(function (err, response, key){
+                expect(response.body.key).toBe('put');
+                cache.get(key, function (err, response) {
+                  expect(response).toBe(null);
+                  done();
+                });
+              }
+            );
+          });
+        }
+      );
+    });
 
-    // it('.get() then .patch() should invalidate cache', function (done) {
-    //   superagent
-    //     .get('localhost:3000/one')
-    //     .end(function (err, response, key){
-    //       expect(response.body.key).toBe('one');
-    //       superagent.cache.get(key, function (err, response) {
-    //         expect(response.body.key).toBe('one');
-    //         superagent
-    //           .patch('localhost:3000/one')
-    //           .end(function (err, response, key){
-    //             expect(response.body.key).toBe('patch');
-    //             superagent.cache.get(key, function (err, response) {
-    //               expect(response).toBe(null);
-    //               done();
-    //             });
-    //           }
-    //         );
-    //       });
-    //     }
-    //   );
-    // });
+    it('.get() then .patch() should invalidate cache', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .use(superagentCache)
+        .end(function (err, response, key){
+          expect(response.body.key).toBe('one');
+          cache.get(key, function (err, response) {
+            expect(response.body.key).toBe('one');
+            superagent
+              .patch('localhost:3000/one')
+              .use(superagentCache)
+              .end(function (err, response, key){
+                expect(response.body.key).toBe('patch');
+                cache.get(key, function (err, response) {
+                  expect(response).toBe(null);
+                  done();
+                });
+              }
+            );
+          });
+        }
+      );
+    });
 
-    // it('.get() then .del() should invalidate cache', function (done) {
-    //   superagent
-    //     .get('localhost:3000/one')
-    //     .end(function (err, response, key){
-    //       expect(response.body.key).toBe('one');
-    //       superagent.cache.get(key, function (err, response){
-    //         expect(response.body.key).toBe('one');
-    //         superagent
-    //           .del('localhost:3000/one')
-    //           .end(function (err, response, key){
-    //             expect(response.body.key).toBe('delete');
-    //             superagent.cache.get(key, function (err, response){
-    //               expect(response).toBe(null);
-    //               done();
-    //             });
-    //           }
-    //         );
-    //       });
-    //     }
-    //   );
-    // });
+    it('.get() then .del() should invalidate cache', function (done) {
+      superagent
+        .get('localhost:3000/one')
+        .use(superagentCache)
+        .end(function (err, response, key){
+          expect(response.body.key).toBe('one');
+          cache.get(key, function (err, response){
+            expect(response.body.key).toBe('one');
+            superagent
+              .del('localhost:3000/one')
+              .use(superagentCache)
+              .end(function (err, response, key){
+                expect(response.body.key).toBe('delete');
+                cache.get(key, function (err, response){
+                  expect(response).toBe(null);
+                  done();
+                });
+              }
+            );
+          });
+        }
+      );
+    });
 
   });
 
